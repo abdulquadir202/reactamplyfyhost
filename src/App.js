140d8a6ce6de749bbe42dcf2f884f509115bd248
Quadir <Abdul.Quadir@atkinsglobal.com>
2020-01-07 13:57:41 +0530

    ll

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import 'react-html5video/dist/styles.css';
 function App() {
   return (
     <div className="App">
-     <Video autoPlay loop muted
+     <Video loop muted
             controls={['PlayPause', 'Seek', 'Time', 'Volume', 'Fullscreen']}
             poster="http://sourceposter.jpg"
             onCanPlayThrough={() => {
@@ -14,7 +14,7 @@ function App() {
             }}>
             <source src="https://s3.amazonaws.com/codecademy-content/courses/React/react_video-fast.mp4" type="video/webm" />
             <track label="English" kind="subtitles" srcLang="en" src="http://source.vtt" default />
-        </Video>
+      </Video>
     </div>
   );
 }
---
+++ src/components/videoList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class VideoList extends React.Component {
+  render() {
+    return <h2>I am a {this.props.color} Car!</h2>;
+  }
+}
+
+export default VideoList;
